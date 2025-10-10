b04fafc86a0fe95f4e45e27c8d030c5658bb4e07
Is-ammar <eammarpr@gmail.com>
2025-10-10 21:42:42 +0100

    fix: deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  // Use relative paths in production so it works under any subpath (GitHub Pages)
   base: command === 'build' ? './' : '/',
   build: {
     outDir: 'dist',
